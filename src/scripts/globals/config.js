7c427bc5a1f256b7211db936c1fe0d689936422b
Richard Alvin <richardalvinpratama8@gmail.com>
2023-02-28 23:17:39 +0700

    workbox and indexed db

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,6 +3,10 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/{pictureId}',
   DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'FastResto',
+  DATABASE_NAME: 'fast-resto-database',
+  DATABASE_VESION: 1,
+  OBJECT_STORE_NAME: 'restos',
 };
 
 export default CONFIG;
--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,13 @@
 import UrlParser from '../../routes/url-parser';
 import RestoDbSource from '../../data/restodb-source';
-import { createRestoDetailTemplate } from '../templates/template-creator';
+import { createRestoDetailTemplate, createLikeButtonTemplate } from '../templates/template-creator';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
 
 const Detail = {
   async render() {
     return `
       <div id="resto" class="resto"></div>
+      <div id="likeButtonContainer"></div>
       `;
   },
 
@@ -14,6 +16,17 @@ const Detail = {
     const resto = await RestoDbSource.detailResto(url.id);
     const restoContainer = document.querySelector('#resto');
     restoContainer.innerHTML = createRestoDetailTemplate(resto);
+    
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: movie.id,
+        title: movie.title,
+        overview: movie.overview,
+        backdrop_path: movie.backdrop_path,
+        vote_average: movie.vote_average,
+      },
+    });
   },
 };
 
